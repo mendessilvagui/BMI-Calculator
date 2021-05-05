3157474f86861cb3e651163226e821c64611d0c9
Guilherme Mendes <mendes.silva.guilherme@gmail.com>
2021-05-05 14:35:30 -0300

    Final changes

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,41 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Guilherme Mendes da Silva on 02/02/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+            
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more.", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "You're OK.", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less.", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        }
+    }
+    
+    func getBMIValue() -> String {
+        let bmiTo1Decimal = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1Decimal
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+}
